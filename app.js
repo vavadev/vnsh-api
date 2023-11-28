c92d0508515810d10ede0b82f2fc30c31a0db7d0
Vladimir <vladimir.yas@gmail.com>
2023-11-28 15:58:21 +0300

    new api

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
 const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = 3000;
 
-app.use(express.static('public'))
+app.get('/', (req, res) => {
+  const url = 'https://www.binance.com/ru/price/bitcoin';
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+  axios.get(url)
+    .then((response) => {
+      if (response.status === 200) {
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        // Пример: извлечение заголовка страницы
+        const pageTitle = $('title').text();
+
+        // Пример: извлечение текста из определенного элемента на странице
+        const price = $('div[data-bn-type="text"]')
+        const priceTitle = $('h2[data-bn-type="text"]').eq(0).html();
+        const priceText = $('div[data-bn-type="text"]').eq(10).html();
+        const currentPriceText = $('div[data-bn-type="text"]').eq(13).html();
+        const changeText = $('div[data-bn-type="text"]').eq(11).html();
+        const dateText = $('div[data-bn-type="text"]').eq(8).html();
+
+        price.each (function() {
+            // console.log($(this).html());
+        })
+        // Создаем объект с данными для отправки в формате JSON
+        const data = {
+          priceTitle,
+          priceText,
+          changeText,
+          currentPriceText,
+          dateText,
+        };
+
+        // Отправляем данные в формате JSON как ответ на запрос
+        res.json(data);
+      } else {
+        console.error('Не удалось получить страницу:', response.status);
+        res.status(500).send('Произошла ошибка');
+      }
+    })
+    .catch((error) => {
+      console.error('Произошла ошибка:', error);
+      res.status(500).send('Произошла ошибка');
+    });
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+  console.log(`Сервер запущен на порту ${port}`);
+});
